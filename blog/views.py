a7f53c9663f74db59583fa7a7ce0718ba5e7373f
Szymon Żaba <szymonzaba18@gmail.com>
2023-11-05 18:28:16 +0100

    all views done

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Comment, Post
 from django.utils import timezone
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from forms import PostForm, CommentForm
 from django.urls import reverse_lazy
+
 
 class AboutView(TemplateView):
     template_name = 'about.html'
@@ -49,3 +50,37 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+@login_required()
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm
+    return render(request, 'blog/comment_form.html', {'form':form})
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    #Taking from Models and changing it from False to True beacause we taking this def
+    comment_approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
